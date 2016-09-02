618dc38d96f95de1b43965201d9561d86f7bb953
avdkishore <avdkishore@avds-MacBook-Air.local>
2016-09-02 09:37:44 +0530

    App setup

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,16 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+
+class CoursesPage extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+        <h1>Courses</h1>
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
